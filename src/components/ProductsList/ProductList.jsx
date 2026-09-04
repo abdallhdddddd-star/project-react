8e4dbe351c5eff0e2984134f5c2feaa5851f1740
abdallah <abdallhdddddd@gmail.com>
2026-09-04 21:03:21 +0300

    loader

--- src/components/ProductsList/ProductList.jsx
+++ src/components/ProductsList/ProductList.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 
 import Card from "../Card/Card";
-import Loader from "../Loader/Loader";
 
 
 export default function ProductList() {
@@ -29,12 +28,7 @@ export default function ProductList() {
     }, []);
 
 
-    if(isloading){
-        return(
-            <Loader/>
-        );
-    }
-
+  
 
     if(error){
         return <div className="alert alert-danger"></div>
